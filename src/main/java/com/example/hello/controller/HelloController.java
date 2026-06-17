24aaf821f210fbe9511fe386e19530c9480f6bcf
GH11PRASANTH <97958269+GH11PRASANTH@users.noreply.github.com>
2026-06-17 16:11:41 +0530

    Update hello message to reflect Java version

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping("/hello")
     @Operation(summary = "Get Hello Message")
     public String hello() {
-        return "Hello World from Spring Boot Java 17 and 21";
+        return "Hello World from Spring Boot Java 17";
     }
 }
